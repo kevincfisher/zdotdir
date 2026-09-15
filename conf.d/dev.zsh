d79fd63f3c59dd576f161af16bbdb9b656b3eb1d
Kevin Fisher <kevincliffordfisher@gmail.com>
2026-09-15 15:23:48 -0400

    fix cargo config location

--- conf.d/dev.zsh
+++ conf.d/dev.zsh
@@ -1,3 +1,6 @@
 # === Dev Settings ===
 # Place things relating to development here (setting path vars for tools and langs etc.)
+
+export CARGO_HOME=${XDG_CONFIG_HOME:-$HOME/.config}/cargo
+echo "$CARGO_HOME"
 . "$CARGO_HOME/.cargo"
